9a9a0c4bf52f7fcaa9035a4fbe6b0e911b4bedcf
Devashish Nema <devnema3@gmail.com>
2024-07-31 14:33:09 +0530

    static/class_variable

--- OOPS/variables.py
+++ OOPS/variables.py
@@ -1,6 +1,14 @@
 # instance variable
-# static/class variable
+# static/class variable 
 # local variable
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# INSTANCE VARIALE
 
 # class student:
    
@@ -54,16 +62,53 @@
 
 
 
-class student:
+# class student:
         
-    def display(self,name,age):
-       self.n1 = name
-       self.n2 = age
-    def show(self):
-        print(self.n1,self.n2)   
+#     def display(self,name,age):
+#        self.n1 = name
+#        self.n2 = age
+#     def show(self):
+#         print(self.n1,self.n2)   
         
         
-obj1=student()
-obj1.display("Deva",23)
-obj1.show()
-print(obj1.n1,obj1.n2)
+# obj1=student()
+# obj1.display("Deva",23)
+# obj1.show()
+# print(obj1.n1,obj1.n2)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# STATIC/CLASS VARIABLE
+
+class Student:
+    
+    # School = "SJCS"
+    
+    def __init__(self,name,roll_no):
+        self.name = name
+        self.roll_no = roll_no
+        
+        # Student.center_code = 101
+        
+    def display(self):
+        
+        Student.grade = "10th"
+        
+        print("name = ",self.name)
+        print("roll_no = ",self.roll_no)
+        # print("School = ",Student.School)
+        # print("center = ",Student.center_code)
+        print("grade = ",Student.grade)
+        
+obj=Student("Deva",62)
+obj.display()
+# print("School",Student.School)        
+
+
+
+# -------------------------------------------------------------------------------------------------------
+
+
+        
