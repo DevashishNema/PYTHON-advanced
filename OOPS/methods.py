0f7e8f39f1772e8f52e6c8a72d62e3c0beb8af5c
Devashish Nema <devnema3@gmail.com>
2024-08-01 14:31:20 +0530

    oops methods

---
+++ OOPS/methods.py
@@ -0,0 +1,106 @@
+# class Student:
+#     def display():
+#         print("hello")
+        
+#     def show():
+#         print("welcome")
+        
+# obj=Student()
+# obj.display()   
+
+
+
+
+
+# class Student:
+#     def display():
+#         print("hello")
+        
+#     def show():
+#         print("welcome")
+        
+# obj=Student()
+# obj.display()        
+# obj.show() 
+
+
+
+
+# class Student:
+#     def display():
+#         print("hello")
+        
+#     def show():
+#         print("welcome")
+        
+# obj=Student
+# obj.display()        
+# obj.show()
+
+
+
+
+
+
+# class Student:
+#     def display(self):
+#         print("hello")
+#     @staticmethod
+#     def show():
+#         print("welcome")
+        
+# obj=Student()
+# obj.display()        
+# obj.show()  
+
+
+
+
+
+
+# class Student:    
+#     def display():
+#         print("hello")
+        
+#     def show():
+#         print("welcome")
+        
+# print(dir(Student))                     #dir use to know variable and methods of class 
+
+
+
+
+
+# class Student: 
+#     x=10 
+#     def __init__(self):
+#         print("hey")
+#     @staticmethod
+#     def display(x,y):
+#         print("hello")
+        
+#     def show():
+#         print("welcome")
+# obj= Student()
+# obj.display(10,20)
+
+# print(Student.__dict__)  
+
+
+
+
+
+class Student: 
+    x=10 
+    def __init__(self):
+        print("hey")
+    @staticmethod
+    def display(x,y):
+        print("hello")
+        
+    def show():
+        print("welcome")
+obj= Student()
+obj.display(10,20)
+
+print(Student.__doc__)                    
